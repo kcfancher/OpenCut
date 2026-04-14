5f2f00dde03d3b31d3824858e4a64fbfee34c9f8
Maze Winther <mazewinther@gmail.com>
2026-04-14 15:14:30 +0200

    chore: hide logos sticker category

--- apps/web/src/constants/sticker-constants.ts
+++ apps/web/src/constants/sticker-constants.ts
@@ -2,7 +2,8 @@ export const STICKER_INTRINSIC_SIZE_FALLBACK = 200;
 
 export const STICKER_CATEGORIES = {
 	all: "All",
-	logos: "Logos",
+	// v0.4.0
+	// logos: "Logos",
 	flags: "Flags",
 	shapes: "Shapes",
 };
